46af4a266d63ccc245ce97d43a4759c7321832ca
Thornton Thomas L <Thomas.Thornton@srpnet.com>
2018-03-05 14:40:57 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Todo } from './todo';
+import { NotificationsService } from 'angular4-notifications';
+import * as Redux from 'redux';
+import { AppStore } from './app-store';
+import { AppState } from './app-state';
+import { 
+  addTodo,
+  updateTodo,
+  removeTodo,
+  clearCompleted,
+  startEdit,
+  cancelEdit,
+  todosRetrieved,
+  toggleCompletion
+} from './actions';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +22,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  newTodoText: string = '';
+  todos: Todo[] = [];
+
+  constructor(private notificationsService: NotificationsService, @Inject(AppStore) private store: Redux.Store<AppState>) {
+    store.subscribe(() => this.updateState());
+    this.updateState();
+  }
+
+  updateState() {
+    const appState: AppState = this.store.getState();
+    if (typeof appState == 'undefined' || appState == null) {
+      return;
+    }
+    this.todos = appState.todos;
+    this.newTodoText = appState.newTodoText;
+  }
+
+  addTodo(): void {
+    if (this.newTodoText.trim().length) {
+      this.store.dispatch(addTodo(this.newTodoText.trim()));
+    }
+  }
+
+  removeTodo(todo: Todo): void {
+    this.store.dispatch(removeTodo(todo));
+  }
+
+	editTodo(index: number) {
+    this.store.dispatch(startEdit(index));
+	}
+
+  toggleCompletion(index: number): void {
+    this.store.dispatch(toggleCompletion(index));
+  }
+
+  getActiveTodos(): Todo[] {
+    return this.todos.filter(todo => !todo.isCompleted);
+  }
+
+  clearCompleted(): void {
+    this.store.dispatch(clearCompleted());
+  }
+
+	cancelEditTodo(index: number): void {
+    this.store.dispatch(cancelEdit(index));
+	}
+
+	updateTodo(index: number, editedTitle: string): void {
+    this.store.dispatch(updateTodo(index, editedTitle));
+	}
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { SimpleNotificationsModule } from 'angular4-notifications';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CompletedComponent } from './completed/completed.component';
-import { EditComponent } from './edit/edit.component';
 
 import { createStore } from 'redux';
 import { AppStore } from './app-store';
@@ -22,28 +17,13 @@ export function storeFactory() {
     return store;
 }
 
-const appRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'completed', component: CompletedComponent },
-  { path: '**', redirectTo: 'home' }
-];
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CompletedComponent,
-    EditComponent
+    AppComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      {enableTracing: true} //typically just for debug
-    ),
     BrowserModule,
     FormsModule,
-    HttpClientModule,
     BrowserAnimationsModule, 
     SimpleNotificationsModule.forRoot()
   ],
